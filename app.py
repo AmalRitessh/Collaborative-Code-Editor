9d7a31d9ae6b05f3900400f3d74333748da03584
AmalRitessh <ashokritessh2004@gmail.com>
2024-11-29 16:16:28 +0530

    added a function to access home.html(temp)

--- app.py
+++ app.py
@@ -10,6 +10,10 @@ app = Flask('__name__')
 def index():
 	return render_template('index.html')
 
+@app.route('/home')
+def home():
+	return render_template('home.html')
+
 @app.route('/python',methods=['POST','GET'])
 def python():
 	if request.method  == 'POST':
